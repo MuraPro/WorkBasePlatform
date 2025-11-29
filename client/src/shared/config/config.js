9e48f980ac35e648fa5ecd50bab3dc2473db3eb0
MuraPro <Chalanamdo@gmail.com>
2025-11-29 20:49:07 +0500

    Remove .env and add .env.example

--- client/src/shared/config/config.js
+++ client/src/shared/config/config.js
@@ -1,5 +1,2 @@
 export const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
-export const IS_FIREBASE = import.meta.env.VITE_IS_FIREBASE === 'true';
-export const FIREBASE_AUTH_URL = import.meta.env.VITE_FIREBASE_AUTH_URL;
-export const FIREBASE_KEY = import.meta.env.VITE_FIREBASE_KEY;
 export const AVATAR_URL = import.meta.env.VITE_AVATAR_URL;
