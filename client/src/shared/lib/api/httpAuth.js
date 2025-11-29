46e948b7eeca1829fea609f45008f706db9627c6
MuraPro <Chalanamdo@gmail.com>
2025-11-29 22:07:05 +0500

    change baseUrl

--- client/src/shared/lib/api/httpAuth.js
+++ client/src/shared/lib/api/httpAuth.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 import { VITE_API_ENDPOINT } from '../../config/config';
 
 export const httpAuth = axios.create({
-  baseURL: VITE_API_ENDPOINT + '/auth/',
+  baseURL: VITE_API_ENDPOINT + 'auth/',
 });
